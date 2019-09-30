2e77af5a2028e84f0cfcd14550ed38b2419cd663
kwangwoo <rhkddn1657@gmail.com>
2019-09-30 23:27:10 +0900

    Applied Viewpager - fragment show / hide 사용 시 fragment가 겹치는 문제가 발생해 viewpager로 수정

--- app/src/main/java/com/android/bottomnavigationtest/activity/MainActivity.java
+++ app/src/main/java/com/android/bottomnavigationtest/activity/MainActivity.java
@@ -7,25 +7,31 @@ import com.android.bottomnavigationtest.R;
 import com.android.bottomnavigationtest.fragment.HomeFragment;
 import com.android.bottomnavigationtest.fragment.SearchFragment;
 import com.android.bottomnavigationtest.fragment.SettingFragment;
+import com.android.bottomnavigationtest.view.CustomViewPager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private BottomNavigationView bottomNavigationView;
+    private CustomViewPager viewPager;
+
 
     private HomeFragment homeFragment = HomeFragment.newInstance();
     private SearchFragment searchFragment = SearchFragment.newInstance();
     private SettingFragment settingFragment = SettingFragment.newInstance();
 
-    private FragmentManager fragmentManager = getSupportFragmentManager();
-
-    private Fragment active = homeFragment;
+    private Fragment active;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,59 +39,83 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.nav_bottomnavigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
-
-        fragmentManager.beginTransaction().add(R.id.container, homeFragment).commit();
-
+        viewPager = findViewById(R.id.view_pager);
+        setBottomNavigationView();
+        setViewPager();
     }
 
+    private void setViewPager() {
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager());
+        fragmentAdapter.add(homeFragment);
+        fragmentAdapter.add(searchFragment);
+        fragmentAdapter.add(settingFragment);
+        viewPager.setAdapter(fragmentAdapter);
+        viewPager.disableScroll(true);
+        viewPager.setCurrentItem(0);
+        viewPager.setOffscreenPageLimit(2);
+    }
+
+    private void setBottomNavigationView() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         switch (menuItem.getItemId()) {
             case R.id.home:
-                showFragment(homeFragment);
+                viewPager.setCurrentItem(0,false);
+                active = homeFragment;
                 return true;
             case R.id.search:
-                showFragment(searchFragment);
+                viewPager.setCurrentItem(1, false);
+                active = searchFragment;
                 return true;
             case R.id.setting:
-                showFragment(settingFragment);
+                viewPager.setCurrentItem(2, false);
+                active = settingFragment;
                 return true;
 
         }
         return false;
     }
 
-
-    private void showFragment(Fragment fragment) {
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
-
-        if (fragmentManager.getFragments().contains(fragment)) {
-            fragmentTransaction.hide(active).show(fragment).commit();
-        }
-        else {
-            fragmentTransaction.add(R.id.container, fragment).hide(active).commit();
-        }
-        active = fragment;
-    }
-
     @Override
     public void onBackPressed() {
 
-        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
-            if (fragment != null && fragment.isVisible()) {
-                if (fragment.getChildFragmentManager().getBackStackEntryCount() > 0) {
-                    fragment.getChildFragmentManager().popBackStack();
-                    return;
-                }
-                else if (!(fragment instanceof HomeFragment)) {
-                    bottomNavigationView.setSelectedItemId(R.id.home);
-                    return;
-                }
-            }
+        if (active.getChildFragmentManager().getBackStackEntryCount() > 0) {
+            active.getChildFragmentManager().popBackStack();
+            return;
         }
+
+        else if (!(active instanceof HomeFragment)) {
+            bottomNavigationView.setSelectedItemId(R.id.home);
+            return;
+        }
+
         super.onBackPressed();
+    }
+
+    public class FragmentAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragments = new ArrayList<>();
+
+        public FragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void add(Fragment fragment) {
+            fragments.add(fragment);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
     }
 
 }
--- app/src/main/java/com/android/bottomnavigationtest/fragment/HomeFragment.java
+++ app/src/main/java/com/android/bottomnavigationtest/fragment/HomeFragment.java
@@ -47,4 +47,6 @@ public class HomeFragment extends Fragment {
 
 
 
+
+
 }
